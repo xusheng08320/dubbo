fff038e9703cd37f00ba89ef0e56954e531d72ee
xusheng <xusheng08320@aliyun.com>
2022-02-09 08:35:12 +0800

    spi机制

--- dubbo-test/dubbo-test-examples/src/main/java/com/alibaba/dubbo/examples/spi/PrintServiceTest.java
+++ dubbo-test/dubbo-test-examples/src/main/java/com/alibaba/dubbo/examples/spi/PrintServiceTest.java
@@ -5,7 +5,7 @@ import com.alibaba.dubbo.common.extension.ExtensionLoader;
 public class PrintServiceTest {
 
     public static void main(String[] args) {
-        PrintService printService = ExtensionLoader.getExtensionLoader(PrintService.class).getDefaultExtension();
+        PrintService printService = ExtensionLoader.getExtensionLoader(PrintService.class).getExtension("impl");
         printService.printInfo();
     }
 
