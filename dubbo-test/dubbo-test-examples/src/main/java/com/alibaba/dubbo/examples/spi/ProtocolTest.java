1368d01ccf41f01aee15c24ef8da020201c9ed9a
xusheng <xusheng08320@aliyun.com>
2022-02-17 09:05:48 +0800

    spi机制

---
+++ dubbo-test/dubbo-test-examples/src/main/java/com/alibaba/dubbo/examples/spi/ProtocolTest.java
@@ -0,0 +1,12 @@
+package com.alibaba.dubbo.examples.spi;
+
+import com.alibaba.dubbo.common.extension.ExtensionLoader;
+import com.alibaba.dubbo.rpc.Protocol;
+
+public class ProtocolTest {
+
+    public static void main(String[] args) {
+        Protocol protocol = ExtensionLoader.getExtensionLoader(Protocol.class).getAdaptiveExtension();
+        System.out.println(protocol);
+    }
+}
